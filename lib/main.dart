240f04044a6e85ff49978a38e617eeb2d1a2a2b0
江心才子 <270628297@qq.com>
2019-06-28 14:52:38 +0800

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/model/post.dart';
 
-void main() => runApp(App());
+List<Post> posts = initData();
+
+void main() {
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('您好'),
-            elevation: 4.0, //默认4.0
+      home: Home(),
+      theme: ThemeData(primaryColor: Colors.yellow
+//          primarySwatch: Colors.white
           ),
-          body: Hello()),
-      theme: ThemeData(
-//        primaryColor: Colors.yellow
-          primarySwatch: Colors.yellow),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+//    return Text(posts[index].title);
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16),
+          Text(posts[index].title, style: Theme.of(context).textTheme.title),
+          Text(posts[index].title, style: Theme.of(context).textTheme.subhead),
+          SizedBox(height: 16)
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('您好'),
+        brightness: Brightness.light,
+        elevation: 4.0, //默认4.0
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: _listItemBuilder,
+      ),
+      backgroundColor: Colors.grey[100],
     );
   }
 }
--- lib/model/post.dart
+++ lib/model/post.dart
@@ -5,3 +5,15 @@ class Post {
   final String author;
   final String imageUrl;
 }
+
+List<Post> initData() {
+  var list = List<Post>();
+  for (var i = 0; i < 20; i++) {
+    list.add(Post(
+        title: 'title${i.toString()}',
+        author: 'author${i.toString()}',
+        imageUrl:
+            'https://i1.hdslb.com/bfs/archive/433c065449f6779490d89787526a5019c5fbba59.jpg@380w_240h_100Q_1c.webp'));
+  }
+  return list;
+}
